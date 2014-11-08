7afa616570256d414b8d69af292a1c99a343338b
Michael Ball <cycomachead@gmail.com>
2014-11-08 03:55:41 -0800

    re-add the theme CSS because idk...its kind of nice

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ llab.llab_path = llab.rootURL + llab.install_directory;
 // reference your custom CSS files, from within llab install directory.
 //  Multiple CSS files is fine, include a separate push for each
 llab.paths.css_files.push('css/bootstrap.min.css');
-// llab.paths.css_files.push('css/bootstrap-theme.min.css');
+llab.paths.css_files.push('css/bootstrap-theme.min.css');
 llab.paths.css_files.push("css/default.css");
 // Syntax Highlighting
 llab.paths.css_files.push("css/tomorrow-night-blue.css");
