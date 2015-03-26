4555fa3ee786d9dc471cee5cda30b356d5238178
Michael Ball <cycomachead@gmail.com>
2015-03-26 13:34:18 -0700

    Cleanup some unneeded course stuff.
    See previous commit if we want to pull the sidebar stuff back in, but for now
    this was dead code.

--- script/curriculum.js
+++ script/curriculum.js
@@ -20,63 +20,11 @@ llab.secondarySetUp = function() {
 
     // This stuff should only happen on curriculum pages
 
-    // llab.step = parseInt(llab.getQueryParameter("step"));
-
     // fix snap links so they run snap
     $("a.run").each(function(i) {
         $(this).attr("target", "_blank");
         $(this).attr('href', llab.getSnapRunURL(this.getAttribute('href')));
     });
-
-    // make the vocab box if necessary
-    // FIXME -- performance
-    if ($("span.vocab").length > 0) {
-        if ($("div.vocab").length === 0) {
-            // it might already exist, in order to have a 'topX' class inserted.
-            $(FULL).append('<div class="vocab"></div>');
-        }
-        var vocabDiv = $("div.vocab");
-        $("span.vocab").each(function(i) {
-            if (!(this.getAttribute('term'))) {
-                this.setAttribute('term', this.innerHTML);
-            }
-            vocabDiv.append('<a href="' + llab.rootURL +
-                '/glossary/view.html?term=' + this.getAttribute('term') +
-                '" target="_vocab">' + this.getAttribute('term') + '</a>');
-        });
-    }
-
-    // make the help box if necessary
-    // FIXME -- performance
-    var helpSpans = $("span.help");
-    if (helpSpans.length > 0) {
-        // TODO clean this up
-        $(FULL).append('<div class="help"></div>');
-        var helpDiv = $("div.help");
-        helpSpans.each(function(i) {
-            if (!(this.getAttribute('topic'))) {
-                this.setAttribute('topic', this.innerHTML);
-            }
-            helpDiv.append('<p><a href="' + llab.rootURL +
-            '/help/view.html?topic=' + this.getAttribute('topic') +
-            '" target="_help">' + this.getAttribute('topic') + '</a></p>');
-        });
-    }
-
-    // move anything that belongs in to the margin there, if necessary
-    // these are the 4 class of divs that matter.
-    // FIXME -- poor performace
-    var marginSelector = ["div.key", "div.warning", "div.help", "div.vocab"];
-    if ($(marginSelector.join(',')).length > 0) {
-        // add the two columns.
-        $(FULL).wrapInner('<div id="mainCol"></div>').prepend('<div id="marginCol"></div>');
-        // this moves the divs over.  Perhaps it could do some smarter ordering
-        // always put vocab at the bottom, for instance.
-        var marginCol = $("#marginCol").get(0);
-        $.each(marginSelector, function(i, divclass) {
-            $(divclass).appendTo(marginCol);
-        });
-    }
 
     // Get the topic file and step from the URL
     llab.file = llab.getQueryParameter("topic");
@@ -86,6 +34,7 @@ llab.secondarySetUp = function() {
         return;
     }
 
+    // TODO: Migrate to newer ajax call.
     var ajaxURL = llab.rootURL + "topic/" + llab.file;
     $.ajax({
         url: ajaxURL,
@@ -102,6 +51,7 @@ llab.secondarySetUp = function() {
         }
     });
 
+    // TODO: Make a function.
     var codeElements = $('pre code');
     if (codeElements.length) {
         var cssFile = llab.paths.css_files.syntax_highlights;
