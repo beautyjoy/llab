5881141f792306ecf38cebf14d38661f61fd3675
Nathaniel Titterton <nate@berkeley.edu>
2015-07-27 23:34:44 -0700

    some brainstorm work setting up bs data model, moving sha1 into the right place, getting brainstorm files into loader.js

--- docs/config-example.js
+++ docs/config-example.js
@@ -27,14 +27,15 @@ llab.install_directory = "llab/";
 // absolute path to llab files -- don't change this
 llab.llab_path = llab.rootURL + llab.install_directory;
 
-// reference your custom CSS files, from within llab install directory.
+// load your custom CSS files:
+//   start without a slash, and these will come from within llab install directory.
+//   start with a slash, and these will come from your server root
+//   start with two slashes and a domain name, and these can come from the interet
 //  Multiple CSS files is fine, include a separate push for each
-// llab.paths.css_files.push('css/3.3.0/bootstrap.min.css');
+
 // llab.paths.css_files.push('css/3.3.0/bootstrap-theme.min.css');
-llab.paths.css_files.push('css/3.3.0/bootstrap-compiled.min.css');
-llab.paths.css_files.push('css/default.css');
-// Syntax Highlighting
-llab.paths.css_files.push("css/tomorrow-night-blue.css");
+//llab.paths.css_files.push("/css/tomorrow-night-blue.css");
+//llab.paths.css_files.push('//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css');
 
 
 
