b4d3d9c564d78b522d07188a68f2c760a81d1115
Michael Ball <cycomachead@gmail.com>
2016-01-28 11:01:53 -0800

    undo the backup now that things are running

--- script/defaults.js
+++ script/defaults.js
@@ -49,9 +49,8 @@ llab.CORSCompliantServers.push("0.0.0.0");
 llab.CORSCompliantServers.push("localhost:8000");
 
 
-// llab.snapRunURLBase = "http://snap.berkeley.edu/snapsource/snap.html#open:";
-
-llab.snapRunURLBase = "http://bjc.edc.org/snapsource/snap.html#open:";
+llab.snapRunURLBase = "http://snap.berkeley.edu/snapsource/snap.html#open:";
+//llab.snapRunURLBase = "http://bjc.edc.org/snapsource/snap.html#open:";
 
 
 
